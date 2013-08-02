1631534daff12346c87759e6c7d52a8221ca9b97
Riley Andrews <riley.andrews@gmail.com>
2013-08-02 02:55:00 -0700

    remove the old rendering path

--- ppu.cpp
+++ ppu.cpp
@@ -143,17 +143,8 @@ uint8_t Ppu::readReg(uint32_t reg)
     return 0;
 }
 
-uint32_t Ppu::getNameTableXOffset()
-{
-    return (regs[CONTROL1_REG] & 0x1) * 256;
-}
 
-uint32_t Ppu::getNameTableYOffset()
-{
-    return ((regs[CONTROL1_REG] & 0x2) >> 1) * 256;
-}
-
-uint8_t Ppu::getColorFromPatternTable(uint16_t patternTable, bool is8x8, int offset, uint32_t x, uint32_t y)
+template<bool is8x8> uint8_t Ppu::getColorFromPatternTable(uint16_t patternTable, int offset, uint32_t x, uint32_t y)
 {
     assert(x < 8);
     uint32_t entrySize = is8x8 ? 16 : 32;
@@ -176,160 +167,13 @@ uint8_t Ppu::getColorFromPatternTable(uint16_t patternTable, bool is8x8, int off
     return color;
 }
 
-uint8_t Ppu::getNameTableColor(uint16_t nameTableAddr, uint16_t patternTableAddr, bool is8x8, uint32_t x, uint32_t y)
-{
-    assert(x < renderWidth);
-    assert(y < renderHeight);
-    
-    uint32_t yPatternSize = is8x8 ? 8 : 16;
-    uint16_t entryAddr = nameTableAddr + (y / yPatternSize) * nameTableWidth + x / 8;
-    uint8_t entry = load(entryAddr);
-    uint8_t color = getColorFromPatternTable(patternTableAddr, is8x8, entry, x % 8, y % yPatternSize);
-    
-    return color;
-}
-
-uint8_t Ppu::getAttributeTablePalette(uint16_t nameTableAddr, uint32_t x, uint32_t y)
-{
-    assert(x < renderWidth);
-    assert(y < renderHeight);
-    
-    uint16_t entryAddr = nameTableAddr + nameTableWidth * nameTableHeight + x / 32 + y / 32 * attrTableWidth;
-    uint8_t entry = load(entryAddr);
-    uint32_t tileOffsetX = (x % 32) / 16;
-    uint32_t tileOffsetY = (y % 32) / 16;
-    uint32_t subNibble = tileOffsetX + tileOffsetY * 2;
-    
-    return (entry >> (2 * subNibble)) & 0x3;
-}
-
-uint32_t Ppu::renderBackgroundPixel(int x, int y, bool& transparent)
-{
-    uint32_t patternTableAddr = getBgPatternTableAddr();
-    
-    uint32_t globalOffsetX = (x + xScrollOrigin + getNameTableXOffset()) % (renderWidth * 2);
-    uint32_t globalOffsetY = (y + yScrollOrigin + getNameTableYOffset()) % (renderHeight * 2);
-    
-    uint32_t nameTableOffset = (globalOffsetY / renderHeight) * 2 + (globalOffsetX / renderWidth);
-    uint32_t nameTableAddr = 0x2000 + 0x400 * nameTableOffset;
-    
-    uint8_t palette = getAttributeTablePalette(nameTableAddr, globalOffsetX % renderWidth, globalOffsetY % renderHeight);
-    uint8_t color = getNameTableColor(nameTableAddr, patternTableAddr, isSpriteSize8x8(), globalOffsetX % renderWidth, globalOffsetY % renderHeight);
-    
-    if (color == 0) {
-        transparent = true;
-    }
-    else {
-        transparent = false;
-    }
-    
-    return getColor(palette, color, false);
-}
-
-/*
 void Ppu::render(uint32_t scanline)
 {
     bool spriteSize8x8 = isSpriteSize8x8();
     uint32_t spriteSize = spriteSize8x8 ? 8 : 16;
     
     // Indicate that all bg elements are transparent
-    memset(pixelBgInFront, 0, sizeof(pixelBgInFront));
     memset(pixelWritten, 0, sizeof(pixelWritten));
-    memset(scanlineBuffer, 0, sizeof(scanlineBuffer));
-    
-    // render sprites
-    if (renderSpritesEnabled()) {
-        // Render up to 8 colliding sprites
-        uint32_t patternTableAddr = getSpritePatternTableAddr();
-        uint32_t renderedSprites = 0;
-        for (uint32_t i = 0; i < maxSpriteCount; i++) {
-            
-            // Determine if the provide collides with the scanline
-            uint32_t sprite = maxSpriteCount - 1 - i;
-            if ((scanline >= (spriteRam[sprite].yCoordMinus1 + 1u)) and
-                (scanline <= (spriteRam[sprite].yCoordMinus1 + 1u + spriteSize - 1u))) {
-            }
-            else {
-                continue;
-            }
-            
-            // Render the sprite
-            bool spriteVerticalFlip = (spriteRam[sprite].attr & (1u << 7)) != 0;
-            bool spriteHorizontalFlip = (spriteRam[sprite].attr & (1u << 6)) != 0;
-            for (int j = 0; j < 8; j++) {
-                uint32_t spriteLine = scanline - (spriteRam[sprite].yCoordMinus1 + 1);
-                if (spriteVerticalFlip) {
-                    spriteLine = spriteSize - 1 - spriteLine;
-                }
-                
-                uint32_t xOffset = j;
-                if (spriteHorizontalFlip) {
-                    xOffset = 8 - 1 - xOffset;
-                }
-                uint32_t color = getColorFromPatternTable(patternTableAddr, spriteSize8x8, spriteRam[sprite].tileIndex, xOffset, spriteLine);
-                
-                // Need to do some bounds checking bullshit here
-                uint32_t xCoordinate = spriteRam[sprite].xCoord + j;
-                if (color and (xCoordinate < renderWidth)) {
-                    scanlineBuffer[xCoordinate] = getColor(spriteRam[sprite].attr & 0x3, color, true);
-                    pixelBgInFront[xCoordinate] = (spriteRam[sprite].attr & (1u << 5)) != 0;
-                    pixelWritten[xCoordinate]   = true;
-                }
-                // Do sprite 0 collision detection
-                if (sprite == 0 and color != 0 and (xCoordinate < renderWidth - 1)) {
-                    bool transparent;
-                    renderBackgroundPixel(xCoordinate, scanline, transparent);
-                    if (!transparent) {
-                        setSprite0Hit();
-                    }
-                }
-            }
-            renderedSprites++;
-            if (renderedSprites >= maxRenderedSpritePerScanline) {
-                setLostSprites();
-                break;
-            }
-        }
-    }
-    
-    // render bg
-    if (renderBackgroundEnabled()) {
-        
-        // render background
-        for (uint32_t i = 0; i < renderWidth; i+=1) {
-            bool transparent = false;
-
-
-            uint32_t pixel = renderBackgroundPixel(i, scanline, transparent);
-            
-            if (!pixelWritten[i]) {
-                scanlineBuffer[i] = pixel;
-            }
-            else if (pixelBgInFront[i] and not transparent) {
-                scanlineBuffer[i] = pixel;
-            }
-        }
-    }
-    
-    // dump pixels to display
-    preRender();
-    for (uint32_t i = 0; i < renderWidth; i++) {
-        setPixel(i, scanline, scanlineBuffer[i]);
-    }
-    postRender();
-}
-*/
-
-void Ppu::renderv2(uint32_t scanline)
-{
-    bool spriteSize8x8 = isSpriteSize8x8();
-    uint32_t spriteSize = spriteSize8x8 ? 8 : 16;
-    
-    // Indicate that all bg elements are transparent
-    //memset(pixelBgInFront, 0, sizeof(pixelBgInFront));
-    memset(pixelWritten, 0, sizeof(pixelWritten));
-    memset(bgScanlineBuffer, 0, sizeof(bgScanlineBuffer));
-    memset(spriteScanlineBuffer, 0, sizeof(spriteScanlineBuffer));
     memset(scanlineBuffer, 0, sizeof(scanlineBuffer));
     
     // render bg
@@ -403,7 +247,13 @@ void Ppu::renderv2(uint32_t scanline)
                 if (spriteHorizontalFlip) {
                     xOffset = 8 - 1 - xOffset;
                 }
-                uint32_t color = getColorFromPatternTable(patternTableAddr, spriteSize8x8, spriteRam[sprite].tileIndex, xOffset, spriteLine);
+                uint32_t color;
+                if (spriteSize8x8) {
+                    color = getColorFromPatternTable<true>(patternTableAddr, spriteRam[sprite].tileIndex, xOffset, spriteLine);
+                }
+                else {
+                    color = getColorFromPatternTable<false>(patternTableAddr, spriteRam[sprite].tileIndex, xOffset, spriteLine);
+                }
                 
                 // Need to do some bounds checking bullshit here
                 uint32_t xCoordinate = spriteRam[sprite].xCoord + j;
@@ -447,7 +297,7 @@ void Ppu::tick()
     // Update vram registers
     if (!isVblank and renderBackgroundEnabled()) {
         if (lineClock == 255 and scanline < 240) {
-            renderv2(scanline);
+            render(scanline);
         }
         if (lineClock == 256) {
             vramYInc();
